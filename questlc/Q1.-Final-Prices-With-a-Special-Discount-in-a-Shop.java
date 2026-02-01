86e080f47849529e905673ac4ce17e10e70b2994
srivastava21ashish <srivastava21ashish@gmail.com>
2026-02-01 20:59:26 +0530

    [Q1] [Time Beats: 99.05%] [Memory Beats: 52.83%] - LeetPush

--- questlc/Q1.-Final-Prices-With-a-Special-Discount-in-a-Shop.java
+++ questlc/Q1.-Final-Prices-With-a-Special-Discount-in-a-Shop.java
@@ -1,20 +1,20 @@
 1class Solution {
 2    public int[] finalPrices(int[] prices) {
 3        int l = prices.length;
-4        int[] nsr = new int[l];
-5        Stack<Integer> st = new Stack<>();
-6        nsr[l-1] = prices[l-1];
-7        for(int i=l-1; i>=0; i--){
-8            while(!st.isEmpty() && prices[i] < prices[st.peek()]){
-9                st.pop();
-10            }
-11            if(st.isEmpty()){
-12                nsr[i] = prices[i];
-13            }else{
-14                nsr[i] = prices[i]-prices[st.peek()];
-15            }
-16            st.push(i);
-17        }
-18        return nsr;
+4        int[] st = new int[l];
+5        int p = -1;
+6        for(int i=l-1; i>=0; i--){
+7            int temp = prices[i];
+8            while(p>-1 && prices[i] < st[p]){
+9                st[p]=0;
+10                p--;
+11            }
+12            if(p!=-1){
+13                prices[i] = prices[i] - st[p];
+14            }
+15            st[++p] = temp;
+16        }
+17        
+18        return prices;
 19    }
 20}
