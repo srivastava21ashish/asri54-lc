8cd3571137114254b14c90fbcba51da2a55c4239
srivastava21ashish <srivastava21ashish@gmail.com>
2026-04-26 19:35:39 +0530

    [206] [Time Beats: 100.00%] [Memory Beats: 83.95%] - LeetPush

---
+++ questlc/Reverse-Linked-List.java
@@ -0,0 +1,24 @@
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode reverseList(ListNode head) {
+13        ListNode prev = null;
+14        ListNode curr = head;
+15
+16        while(curr != null){
+17            ListNode nn = curr.next;
+18            curr.next = prev;
+19            prev = curr;
+20            curr = nn;
+21        }
+22        return prev;
+23    }
+24}
